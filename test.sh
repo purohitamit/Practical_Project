7af1f89ccd439d80c8a444ae2247cd40e126045f
Amit Purohit <purohit_amit1000@rediff.com>
2022-04-06 11:04:33 +0000

    fixing Jenkinsfile

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ do
   python3 -m venv venv
   source venv/bin/activate
   pip3 install -r test_requirements.txt
-  python3 -m pytest --cov=application --cov-report term-missing  no:warnings --cov-report=html
+  python3 -m pytest --cov=application --cov-report term-missing --cov-report=html  -p no:warnings 
   deactivate
   cd ..
 done
